50f1a5a77b3301c908aa54ecaa1cc263a62ead56
Logosorfe <141771854+Logosorfe@users.noreply.github.com>
2026-07-02 23:36:48 +0300

    Update ConsoleMenu.java
    Initial commit.

--- src/main/java/org/example/menu/ConsoleMenu.java
+++ src/main/java/org/example/menu/ConsoleMenu.java
@@ -1,25 +1,64 @@
-package org.example.menu;
+package two.menu;
 
-import org.example.model.Animal;
-import org.example.shelter.Shelter;
+import two.model.*;
+import two.shelter.Shelter;
 
 import java.util.Scanner;
 
 public class ConsoleMenu {
     private final Shelter<Animal> shelter;
-    private final Scanner scanner =  new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
+
     public ConsoleMenu(Shelter<Animal> shelter) {
         this.shelter = shelter;
     }
 
-    public void start(){
-        // TODO:
-        // Show menu in a loop
-        // Read user input
-        // Call correct 'Shelter' methods based on selected option
+    public void start() {
+        while (true) {
+            printMenu();
+            int input = scanner.nextInt();
+            switch (input) {
+                case 1:
+                    System.out.println("Enter one of the following animal species:\nCat\nDog\nBird");
+                    String animalSpecies = scanner.next();
+                    System.out.println("Enter animal name: ");
+                    String name = scanner.next();
+                    System.out.println("Enter animal age: ");
+                    int age = scanner.nextInt();
+                    if (animalSpecies.equalsIgnoreCase("Cat"))
+                        shelter.addAnimal(new Cat(new AnimalId(), name, age));
+                    else if (animalSpecies.equalsIgnoreCase("Dog"))
+                        shelter.addAnimal(new Dog(new AnimalId(), name, age));
+                    else if (animalSpecies.equalsIgnoreCase("Bird"))
+                        shelter.addAnimal(new Bird(new AnimalId(), name, age));
+                    else System.out.println("Invalid input(s)");
+                    break;
+                case 2:
+                    System.out.println(shelter.getAllAnimals());
+                    break;
+                case 3:
+                    System.out.println("Enter one of the following animal species:\nCat\nDog\nBird");
+                    String species = scanner.next();
+                    System.out.println(shelter.findBySpecies(species));
+                    break;
+                case 4:
+                    System.out.println(shelter.findAvailableAnimals());
+                    break;
+                case 5:
+                    System.out.println("Enter animal id:");
+                    String animalId = scanner.next();
+                    shelter.markAsAdopted(animalId);
+                    System.out.println("Animal with id " + animalId + " marked as adopted");
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid input");
+            }
+        }
     }
 
-    private void printMenu(){
+    private void printMenu() {
         System.out.println("""
                 1. Add animal
                 2. List all animals
