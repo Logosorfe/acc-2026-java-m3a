670d67727edde3f902b2c07bd7dc8aca2c9e0f2f
Logosorfe <141771854+Logosorfe@users.noreply.github.com>
2026-07-02 23:45:57 +0300

    Update MenuOption.java

--- src/main/java/org/example/menu/MenuOption.java
+++ src/main/java/org/example/menu/MenuOption.java
@@ -1,5 +1,5 @@
 /**PLEASE ADVISE ME ON HOW TO IMPLEMENT THIS CLASS, AS I FOUND NO USE OF IT. THX!*/
-package two.menu;
+package org.example.menu;
 
 public record MenuOption(int number, String label) {
 }
